1d25665b93fd836b64c83e62beeb254f26c6fa06
Fernando Rego <fernandoluisrego@gmail.com>
2023-09-30 17:17:37 +0100

    Add prints

--- calc_server/server.rb
+++ calc_server/server.rb
@@ -8,22 +8,30 @@ require 'calculator_services_pb'
 class CalculatorServer < Calculator::Calculator::Service
   # sum implements the Sum rpc method.
   def sum(calc_req, _unused_call)
-    Calculator::CalcReply.new(res: calc_req.x + calc_req.y)
+    res = calc_req.x + calc_req.y
+    p "<inf> sum: #{calc_req.x} + #{calc_req.y} = #{res}"
+    Calculator::CalcReply.new(res: res)
   end
 
   # sub implements the Sub rpc method.
   def sub(calc_req, _unused_call)
-    Calculator::CalcReply.new(res: calc_req.x - calc_req.y)
+    res = calc_req.x - calc_req.y
+    p "<inf> sub: #{calc_req.x} - #{calc_req.y} = #{res}"
+    Calculator::CalcReply.new(res: res)
   end
 
   # mul implements the Mul rpc method.
   def mul(calc_req, _unused_call)
-    Calculator::CalcReply.new(res: calc_req.x * calc_req.y)
+    res = calc_req.x * calc_req.y
+    p "<inf> mul: #{calc_req.x} * #{calc_req.y} = #{res}"
+    Calculator::CalcReply.new(res: res)
   end
 
   # div implements the Div rpc method.
   def div(calc_req, _unused_call)
-    Calculator::CalcReply.new(res: calc_req.x / calc_req.y)
+    res = calc_req.x / calc_req.y
+    p "<inf> div: #{calc_req.x} / #{calc_req.y} = #{res}"
+    Calculator::CalcReply.new(res: res)
   end
 end
 
